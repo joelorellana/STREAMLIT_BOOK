222662efb415171c93615f9071add81f0d33a0a5
Joel Orellana <joelorellana989@gmail.com>
2023-11-24 08:24:03 -0600

    CH4 ENDED.

--- CH4/penguin_ml/hugging_demo.py
+++ CH4/penguin_ml/hugging_demo.py
@@ -13,3 +13,31 @@ if text:
     result = model(text)
     st.write("Sentiment: ", result[0]["label"])
     st.write("Confidence: ", result[0]["score"])
+
+### SENTIMENT ANALYSIS WITH OPENAI
+import openai
+
+st.title("OpenAI Version")
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+system_message_default = """You are a helpful sentiment analysis assistant.You always respond with the sentiment of the text you are given and the confidence of your sentiment analysis with a number between 0 and 1."""
+
+system_message = st.text_area(
+    "Enter a System Message to instruct OpenAI", system_message_default
+)
+
+analyze_button = st.button("Analyze Text with OpenAI")
+
+if analyze_button:
+    messages = [
+        {"role": "system",
+         "content": f"{system_message}"},
+        {"role": "user",
+         "content": f"Sentiment Analysis of the following text: {text}"},
+    ]
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+    )
+    sentiment = response.choices[0].message.content.strip()
+    st.write(sentiment)
